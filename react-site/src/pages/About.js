0d48bf817054803c4af7d05d38096a2391f3b6c3
tulXoro <tulxoro@hotmail.com>
2022-12-23 00:56:13 -0800

    updated about page

--- react-site/src/pages/About.js
+++ react-site/src/pages/About.js
@@ -25,17 +25,16 @@ class About extends React.Component {
 					<div className="about-content">
 						<p className="about-text">
 							I am a software developer with a passion for creating and designing
-							applications. I am currently a student at the University of
-							California, Irvine, pursuing a B.S. in Computer Science. I am
-							currently looking for a full-time software engineering position
-							starting in Summer 2021.
+							applications. I am currently a student at San Francisco State University
+							pursuing a Bachelor's degree in Computer Science. I am also currently
+							working as a mentor/coding coach at thecoderschool.com.
 						</p>
 						<p className="about-text">
-							I am a self-taught developer who has been programming for over 5
-							years. I have experience with a variety of programming languages
+							I am a self-motivated individual who is able to work independently. 
+							I have experience with a variety of programming languages
 							and frameworks, including Java, C++, Python, JavaScript, React,
 							Node.js, and more. I am also experienced with web development,
-							including HTML, CSS, and Bootstrap.
+							including HTML, CSS.
 						</p>
 						<p className="about-text">
 							I am a hard-working individual who is passionate about creating
