45a889e397f28e58327e47e636db15a4276c32d5
tulXoro <tulxoro@hotmail.com>
2022-12-23 01:34:21 -0800

    did stuff i dont really remember

--- react-site/src/components/Sidebar.js
+++ react-site/src/components/Sidebar.js
@@ -1,4 +1,6 @@
 import { React } from "react";
+import { GitHub, LinkedIn } from '@mui/icons-material';
+import { SvgIcon } from '@mui/material';
 
 import "../style/Sidebar.css";
 
@@ -8,6 +10,22 @@ const Sidebar = () => {
     return (
         <div className="sidebar">
             <img src="/hero.jfif" alt="Normal" />
+            <div className="side-bar-wrapper">
+
+                <h1>Tim Lor</h1>
+                <p>Software Developer</p>
+                <div className="socials">
+                    <a className="social-icons" href="https://github.com/tulxoro" target="_blank" rel="noreferrer">
+                        <SvgIcon component={ GitHub } />
+                        <span className="social-links" >tulXoro</span>
+                    </a>
+
+                    <a className="social-icons" href="https://www.linkedin.com/in/tulxoro/" target="_blank" rel="noreferrer">
+                        <SvgIcon component={ LinkedIn } />
+                        <span className="social-links" >Tim Lor</span>
+                    </a>       
+                </div>
+            </div>
         </div>
     );
 };
